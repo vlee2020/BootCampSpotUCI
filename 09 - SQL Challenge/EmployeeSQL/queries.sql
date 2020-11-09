bcb800bcd71a7c9bdf0608c37434ef06cd1ca3e8
vlee <findvanessalee@gmail.com>
2020-11-08 21:36:14 -0800

    Add queries.sql

---
+++ 09 - SQL Challenge/EmployeeSQL/queries.sql
@@ -0,0 +1,39 @@
+--
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+
+--Query #1: List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+
+
+--Query #2: List first name, last name, and hire date for employees who were hired in 1986.
+
+
+
+--Query #3: List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+
+--Query #4: List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+
+--Query #5: List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+
+--Query #6: List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+
+--Query #7: List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+
+--Query #8: In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+
