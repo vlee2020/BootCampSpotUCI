345c8a23ff40a9a087abc3d44e008d7cdc6900e1
vlee <findvanessalee@gmail.com>
2020-11-10 18:38:29 -0800

    schema v2 w composite keys

--- 09 - SQL Challenge/EmployeeSQL/db_schema.sql
+++ 09 - SQL Challenge/EmployeeSQL/db_schema.sql
@@ -1,5 +1,7 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/NOXday
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR(5)   NOT NULL,
@@ -11,12 +13,18 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(5)   NOT NULL
+    "dept_no" VARCHAR(5)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(5)   NOT NULL,
-    "emp_no" INT   NOT NULL
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 CREATE TABLE "employees" (
@@ -34,7 +42,10 @@ CREATE TABLE "employees" (
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no","salary"
+     )
 );
 
 CREATE TABLE "titles" (
@@ -45,7 +56,7 @@ CREATE TABLE "titles" (
      )
 );
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp__emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
@@ -60,13 +71,3 @@ REFERENCES "titles" ("title_id");
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
---Added following composite keys to tables w/o primary keys outside of QuickDBD
-
-ALTER TABLE dept_emp
-ADD CONSTRAINT CompKey_dept_emp PRIMARY KEY (emp_no, dept_no);
-
-ALTER TABLE dept_manager
-ADD CONSTRAINT CompKey_dept_mgr PRIMARY KEY (dept_no, emp_no);
-
-ALTER TABLE salaries
-ADD CONSTRAINT CompKey_salaries PRIMARY KEY (emp_no, salary);
